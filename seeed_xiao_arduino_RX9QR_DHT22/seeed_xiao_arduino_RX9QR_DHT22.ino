4f1bd8a2f99fe3211c1858ab89c391c9a784a266
elehobica <elehobica@gmail.com>
2020-05-01 17:33:41 +0900

    feat: Supported COM command and calibration loading/storing from/to flash

--- seeed_xiao_arduino_RX9QR_DHT22/seeed_xiao_arduino_RX9QR_DHT22.ino
+++ seeed_xiao_arduino_RX9QR_DHT22/seeed_xiao_arduino_RX9QR_DHT22.ino
@@ -1,3 +1,20 @@
+/*
+Flash total size: 256KB
+number of page 4096
+page size 64Bytes
+Use 0x0003FFC0 ~ 0x0003FFFF (64Bytes) as config area
+*/
+#include "FlashStorage.h" // Atmel SAMD Native Library
+FlashClass flash((const void *) 0x00000000, 256*1024);
+#define CONFIG_AREA_BASE    ((const volatile void *) 0x0003FFC0)
+#define CONFIG_AREA_SIZE    64
+#define CONFIG_USED_SIZE    0x10
+#define CONFIG_BOOT_COUNT   ((const volatile void *) (CONFIG_AREA_BASE + 0x00))
+#define CONFIG_CALIB_COUNT ((const volatile void *) (CONFIG_AREA_BASE + 0x04))
+#define CONFIG_CAL_A        ((const volatile void *) (CONFIG_AREA_BASE + 0x08))
+#define CONFIG_CAL_B        ((const volatile void *) (CONFIG_AREA_BASE + 0x0C))
+uint32_t boot_count, calib_count;
+
 /* Seeed XIAO LED */
 #define LED_pin 13 // PA17_W13 pin
 
@@ -32,8 +49,8 @@ DHT dht(DHTPIN, DHTTYPE);
 #define mein 120 //Automotive: 120, Home or indoor: 1440
 
 //CO2 calibrated number
-float cal_A = 409.1; // you can take the data from RX-9 bottom side QR data #### of first 4 digits. you type the data to cal_A as ###.#
-float cal_B = 61.28; // following 4 digits after cal_A is cal_B, type the data to cal_B as ##.##
+float cal_A = 372.1; // you can take the data from RX-9 bottom side QR data #### of first 4 digits. you type the data to cal_A as ###.#
+float cal_B = 63.27; // following 4 digits after cal_A is cal_B, type the data to cal_B as ##.##
 
 //CO2 Step range
 #define cr1  700      // Base_line ~ cr1
@@ -56,6 +73,13 @@ RX9QR RX9(cal_A, cal_B, Base_line, meti, mein, cr1, cr2, cr3, cr4);
 unsigned int time_now = 0;
 unsigned int time_prev = 0;
 unsigned int interval = 1;
+
+// Serial Read
+char rcv_msg[32] = {};
+int rcv_msg_pos = 0;
+bool rcv_end = false;
+bool enable_monitor = true;
+bool enable_echoback = false;
 
 bool getCO2(unsigned int *val) {
   int status_sensor = 0;
@@ -98,7 +122,59 @@ bool getHumidity(float *val) {
   return !isnan(h);
 }
 
+void updateFlashConfig() {
+  uint32_t *data = (uint32_t *) malloc(CONFIG_AREA_SIZE);
+  for (int i = 0; i < CONFIG_AREA_SIZE/4; i++) {
+    flash.read(CONFIG_AREA_BASE + i*4, &data[i], sizeof(data[i]));
+  }
+  flash.erase(CONFIG_AREA_BASE, CONFIG_AREA_SIZE);
+  flash.write(CONFIG_BOOT_COUNT, &boot_count, sizeof(boot_count));
+  flash.write(CONFIG_CALIB_COUNT, &calib_count, sizeof(calib_count));
+  flash.write(CONFIG_CAL_A, &cal_A, sizeof(cal_A));
+  flash.write(CONFIG_CAL_B, &cal_B, sizeof(cal_B));
+  for (int i = CONFIG_USED_SIZE/4; i < CONFIG_AREA_SIZE/4; i++) {
+    flash.write(CONFIG_AREA_BASE + i*4, &data[i], sizeof(data[i]));
+  }
+  free(data);
+}
+
 void setup() {
+  // Serial COM
+  SerialUSB.begin(115200);
+  delay(1000);
+
+  // Load Config Parameters from flash
+  flash.read(CONFIG_BOOT_COUNT, &boot_count, sizeof(boot_count));
+  flash.read(CONFIG_CALIB_COUNT, &calib_count, sizeof(calib_count));
+  if (boot_count == 0xffffffff) {
+    // Config Area Initialize
+    boot_count = 0;
+    calib_count = 0;
+    flash.erase(CONFIG_AREA_BASE, CONFIG_AREA_SIZE);
+    flash.write(CONFIG_BOOT_COUNT, &boot_count, sizeof(boot_count));
+    flash.write(CONFIG_CALIB_COUNT, &calib_count, sizeof(calib_count));
+    flash.write(CONFIG_CAL_A, &cal_A, sizeof(cal_A));
+    flash.write(CONFIG_CAL_B, &cal_B, sizeof(cal_B));
+  } else {
+    if (boot_count < 0xfffffffe) boot_count++;
+    // Load Calibration data
+    flash.read(CONFIG_CALIB_COUNT, &calib_count, sizeof(calib_count));
+    flash.read(CONFIG_CAL_A, &cal_A, sizeof(cal_A));
+    flash.read(CONFIG_CAL_B, &cal_B, sizeof(cal_B));
+    // update boot_count
+    updateFlashConfig();
+  }
+  SerialUSB.println("");
+  SerialUSB.println("Seeed XIAO CO2/Temp/Humidity Monitor ver 1.00");
+  SerialUSB.print("Boot count: ");
+  SerialUSB.print(boot_count);
+  SerialUSB.print(" Calibration count: ");
+  SerialUSB.println(calib_count);
+  SerialUSB.print("cal_A: ");
+  SerialUSB.print(cal_A);
+  SerialUSB.print(" cal_B: ");
+  SerialUSB.println(cal_B);
+
   // LED
   pinMode(LED_pin, OUTPUT);
   //digitalWrite(LED_pin, LOW);
@@ -108,8 +184,6 @@ void setup() {
   // DHT22 (Temp/Humidity) Sensor Setting
   pinMode(DHTPIN, INPUT);
   dht.begin();
-  // Serial COM
-  SerialUSB.begin(115200);
 }
 
 void loop() {
@@ -126,26 +200,88 @@ void loop() {
     co2_valid = getCO2(&co2_ppm);
     t_valid = getTemperature(&t);
     h_valid = getHumidity(&h);
-    SerialUSB.print("C:");
-    if (co2_valid) {
-      SerialUSB.print(co2_ppm);
-    } else {
-      SerialUSB.print("xxxx");      
+    if (enable_monitor) {
+      SerialUSB.print("# C:");
+      if (co2_valid) {
+        SerialUSB.print(co2_ppm);
+      } else {
+        SerialUSB.print("xxxx");
+      }
+      SerialUSB.print(" T:");
+      if (t_valid) {
+        SerialUSB.print(t);
+      } else {
+        SerialUSB.print("xxxx");
+      }
+      SerialUSB.print(" H:");
+      if (h_valid) {
+        SerialUSB.print(h);
+      } else {
+        SerialUSB.print("xxxx");
+      }
+      SerialUSB.println(""); //CR LF
     }
-    SerialUSB.print(" T:");
-    if (t_valid) {
-      SerialUSB.print(t);
-    } else {
-      SerialUSB.print("xxxx");
-    }
-    SerialUSB.print(" H:");
-    if (h_valid) {
-      SerialUSB.print(h);
-    } else {
-      SerialUSB.print("xxxx");      
-    }
-    SerialUSB.println(""); //CR LF
   } else if (time_now - time_prev >= interval*1000/8) {
     digitalWrite(LED_pin, HIGH); // LED Off
   }
+  // Serial Read
+  while (SerialUSB.available()) {
+    int inByte = SerialUSB.read();
+    if (inByte == '\r' || inByte == '\n' || rcv_msg_pos >= 31) {
+      rcv_msg[rcv_msg_pos] = '\0';
+      rcv_end = true;
+      break;
+    } else {
+      rcv_msg[rcv_msg_pos++] = inByte;
+    }
+  }
+  if (rcv_end && rcv_msg_pos > 0) {
+    //SerialUSB.println(rcv_msg);
+    rcv_msg_pos = 0;
+    rcv_end = false;
+    String rcvMsg = rcv_msg;
+    if (rcvMsg.equalsIgnoreCase("enable_monitor")) {
+      enable_monitor = true;
+    } else if (rcvMsg.equalsIgnoreCase("disable_monitor")) {
+      enable_monitor = false;
+    } else if (rcvMsg.equalsIgnoreCase("enable_echoback")) {
+      enable_echoback = true;
+    } else if (rcvMsg.equalsIgnoreCase("disable_echoback")) {
+      enable_echoback = false;
+    } else if (rcvMsg.equalsIgnoreCase("reset") || rcvMsg.equalsIgnoreCase("reboot")) {
+      NVIC_SystemReset();
+    } else if (rcvMsg.substring(0, 6).equalsIgnoreCase("calib ")) {
+      if (rcvMsg.length() == 6+21 || rcvMsg.length() == 6+22) {
+        // QR-code example "calib 15742214167K0544CAB07A"
+        String factor_A = rcvMsg.substring(6, 10);
+        String factor_B = rcvMsg.substring(10, 14);
+        String comp_factor = rcvMsg.substring(14, 17);
+        String serial_number = rcvMsg.substring(17, 28); // Officially it's 22 char. but there are modules which have 21 char.
+        SerialUSB.print("cal_A: ");
+        SerialUSB.println(factor_A);
+        SerialUSB.print("cal_B: ");
+        SerialUSB.println(factor_B);
+        SerialUSB.print("Temp comp: ");
+        SerialUSB.println(comp_factor);
+        SerialUSB.print("Serial Number: ");
+        SerialUSB.println(serial_number);
+        if (comp_factor.equals("167") && serial_number.charAt(0) == 'K') {
+          // Update Calibration parameter in flash
+          cal_A = factor_A.toFloat() / 10.0;
+          cal_B = factor_B.toFloat() / 100.0;
+          calib_count++;
+          updateFlashConfig();
+          SerialUSB.println("Calibration OK");
+        } else {
+          SerialUSB.println("Calibration Value Error");
+        }
+      } else {
+        SerialUSB.println("Calibration Format Error");
+      }
+    }
+    if (enable_echoback) {
+      SerialUSB.println(rcvMsg);
+    }
+  }
+  delay(10);
 }
