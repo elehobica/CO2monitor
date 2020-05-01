e87b2673d8b72194b006f201601beddb09c23338
elehobica <elehobica@gmail.com>
2020-05-01 11:33:02 +0900

    feat: Supported Temperature & Humidity capture by DHT22

--- seeed_xiao_arduino_RX9QR_DHT22/seeed_xiao_arduino_RX9QR_DHT22.ino
+++ seeed_xiao_arduino_RX9QR_DHT22/seeed_xiao_arduino_RX9QR_DHT22.ino
@@ -1,12 +1,16 @@
-#include <LiquidCrystal_I2C.h>
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 2;
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
+/* Seeed XIAO LED */
+#define LED_pin 13 // PA17_W13 pin
 
-/* RX-9 QR Sample Code
+/* DHT22 Temperature/Humidity Sensor */
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+//create an instance of DHT sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+/* ========== CO2 Sensor RX-9 (Begin) ========== */
+/* https://github.com/EXSEN/RX-9 */
+/* Utilizing RX-9 QR Sample Code
  *  date: 2020.03.04
  *  Carbon Dioxide Gas sensor(RX-9) with
  *  ATMEGA328p, 16Mhz, 5V
@@ -19,8 +23,8 @@ LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
  *  V: 3.3V > 200 mA
  */
 #include "RX9QR.h"
-#define EMF_pin 5   // RX-9 E with A0 of arduino
-#define THER_pin 6  // RX-9 T with A1 of arduino
+#define EMF_pin 5   // RX-9 E with PA5
+#define THER_pin 6  // RX-9 T with PA6
 #define ADCvolt 3.3
 #define ADCResol 1024
 #define Base_line 432
@@ -45,89 +49,103 @@ float cal_B = 61.28; // following 4 digits after cal_A is cal_B, type the data t
 #define C3 0.00000002113323296
 float Resist_0 = 15;
 
-//Timing
-unsigned int time_s = 0;
-unsigned int time_s_prev = 0;
-unsigned int time_s_set = 1;
-
-extern volatile unsigned long timer0_millis;
-
-//CO2 Value
-int status_sensor = 0;
-unsigned int co2_ppm = 0;
-unsigned int co2_step = 0;
-float EMF = 0;
-float THER = 0;
-
 RX9QR RX9(cal_A, cal_B, Base_line, meti, mein, cr1, cr2, cr3, cr4);
+/* ========== CO2 Sensor RX-9 (End) ========== */
+
+//Timing
+unsigned int time_now = 0;
+unsigned int time_prev = 0;
+unsigned int interval = 1;
+
+bool getCO2(unsigned int *val) {
+  int status_sensor = 0;
+  unsigned int co2_ppm = 0;
+  unsigned int co2_step = 0;
+  float EMF = 0;
+  float THER = 0;
+
+  //read EMF data from RX-9, RX-9 Simple START-->
+  EMF = analogRead(EMF_pin);
+  delay(1);
+  EMF = EMF / (ADCResol - 1);
+  EMF = EMF * ADCvolt;
+  EMF = EMF / 6;
+  EMF = EMF * 1000;
+  // <-- read EMF data from RX-9, RX-9 Simple END 
+  
+  //read THER data from RX-9, RX-9 Simple START-->
+  THER = analogRead(THER_pin);
+  delay(1);
+  THER = 1/(C1+C2*log((Resist_0*THER)/(ADCResol-THER))+C3*pow(log((Resist_0*THER)/(ADCResol-THER)),3))-273.15;
+  // <-- read THER data from RX-9, RX-9 Simple END
+  
+  status_sensor = RX9.status_co2();   //read status_sensor, status_sensor = 0 means warming up, = 1 means stable
+  co2_ppm = RX9.cal_co2(EMF,THER);    //calculation carbon dioxide gas concentration. 
+  co2_step = RX9.step_co2();          //read steps of carbon dioixde gas concentration. you can edit the step range with cr1~cr4 above.
+  *val = co2_ppm;
+  return (status_sensor == 1);
+}
+
+bool getTemperature(float *val) {
+  float t = dht.readTemperature();
+  *val = t;
+  return !isnan(t);
+}
+
+bool getHumidity(float *val) {
+  float h = dht.readHumidity();
+  *val = h;
+  return !isnan(h);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  // LED
+  pinMode(LED_pin, OUTPUT);
+  //digitalWrite(LED_pin, LOW);
+  // CO2 Sensor Pin Setting
+  pinMode(EMF_pin, INPUT);
+  pinMode(THER_pin, INPUT);
+  // DHT22 (Temp/Humidity) Sensor Setting
+  pinMode(DHTPIN, INPUT);
+  dht.begin();
+  // Serial COM
   SerialUSB.begin(115200);
-  // initialize LCD
-  //lcd.init();
-  // turn on LCD backlight                      
-  //lcd.backlight();
 }
 
 void loop() {
-  /*
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
-  */
   // put your main code here, to run repeatedly:
-  time_s = millis()/1000;
-  if(time_s - time_s_prev >= time_s_set){
-    time_s_prev = time_s;
+  time_now = millis();
+  if(time_now - time_prev >= interval*1000){
     //every 1 sec
-    //read EMF data from RX-9, RX-9 Simple START-->
-    EMF = analogRead(EMF_pin);
-    delay(1);
-    EMF = EMF / (ADCResol - 1);
-    EMF = EMF * ADCvolt;
-    EMF = EMF / 6;
-    EMF = EMF * 1000;
-    // <-- read EMF data from RX-9, RX-9 Simple END 
+    time_prev += interval*1000;
+    digitalWrite(LED_pin, LOW); // LED On
 
-    //read THER data from RX-9, RX-9 Simple START-->
-    THER = analogRead(THER_pin);
-    delay(1);
-    THER = 1/(C1+C2*log((Resist_0*THER)/(ADCResol-THER))+C3*pow(log((Resist_0*THER)/(ADCResol-THER)),3))-273.15;
-    // <-- read THER data from RX-9, RX-9 Simple END
-    
-    status_sensor = RX9.status_co2();   //read status_sensor, status_sensor = 0 means warming up, = 1 means stable
-    co2_ppm = RX9.cal_co2(EMF,THER);    //calculation carbon dioxide gas concentration. 
-    co2_step = RX9.step_co2();          //read steps of carbon dioixde gas concentration. you can edit the step range with cr1~cr4 above.
-    SerialUSB.print("# ");                //Starting letter
-    if(co2_ppm <1000){
-      SerialUSB.print("0");
+    bool co2_valid, t_valid, h_valid;
+    unsigned int co2_ppm;
+    float t, h;
+    co2_valid = getCO2(&co2_ppm);
+    t_valid = getTemperature(&t);
+    h_valid = getHumidity(&h);
+    SerialUSB.print("C:");
+    if (co2_valid) {
+      SerialUSB.print(co2_ppm);
+    } else {
+      SerialUSB.print("xxxx");      
     }
-    else{      
+    SerialUSB.print(" T:");
+    if (t_valid) {
+      SerialUSB.print(t);
+    } else {
+      SerialUSB.print("xxxx");
     }
-    SerialUSB.print(co2_ppm); SerialUSB.print(" ");
-    /*
-    if(status_sensor){
-      SerialUSB.print("NR");
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("CO2: ");
-      lcd.print(co2_ppm);
-      lcd.print(" ppm");
+    SerialUSB.print(" H:");
+    if (h_valid) {
+      SerialUSB.print(h);
+    } else {
+      SerialUSB.print("xxxx");      
     }
-    else{
-      SerialUSB.print("WU");
-      //it takes about 3 minutes to heating the sensor.
-      //while warming up. data is not correct. 
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Warming Up");
-      lcd.setCursor(0, 1);
-      lcd.print(co2_ppm);
-      lcd.print(" ppm");
-    }
-    */
     SerialUSB.println(""); //CR LF
+  } else if (time_now - time_prev >= interval*1000/8) {
+    digitalWrite(LED_pin, HIGH); // LED Off
   }
 }
